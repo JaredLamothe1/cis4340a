6d485afe9b967d8a0c86061d754fb23f086ee07e
JaredLamothe1 <161624495+JaredLamothe1@users.noreply.github.com>
2024-03-03 16:39:01 -0500

    Create Rule6.java

---
+++ Rule6.java
@@ -0,0 +1,13 @@
+try {
+  // ...
+} catch (SecurityException se) {
+  System.err.println(se);
+  // Recover from exception
+}
+
+try {
+  // ...
+} catch(SecurityException se) {
+  logger.log(Level.SEVERE, se);
+  // Recover from exception
+}
